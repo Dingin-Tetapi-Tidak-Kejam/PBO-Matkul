62b4263164448f9d9025f7ab9e53148a0fd28ca4
diosadellafortuna <diosadellafortuna@gmail.com>
2026-06-02 12:13:18 +0700

    update

---
+++ cashflow-web/src/main/java/cashflow/controller/SpaController.java
@@ -0,0 +1,13 @@
+package cashflow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpaController {
+
+    @RequestMapping(value = {"/", "/login", "/signup", "/dashboard"})
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
